e480e403ad84d7824a9a093b153efdbfeca97961
LiamSpruyt <60923317+LiamSpruyt@users.noreply.github.com>
2025-05-15 12:30:04 +0200

    Add files via upload

---
+++ js/leaflet.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([51.246154, 4.458460], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.246154, 4.458460]).addTo(map);
